ef6d865e1b6299d4c27ad9576c7dcf5fd3d92618
jv-arc <jvarc.gm@gmail.com>
2025-09-19 13:09:15 -0300

    script de inicializacao para lidar com as chaves de api

---
+++ run.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ -f ./.venv/bin/activate ]]; then
+  source ./.venv/bin/activate
+else
+  echo "Error: Virtualenv not found at ./.venv/bin/activate" >&2
+  exit 1
+fi
+
+if [[ -f .env ]]
+ then
+    set -o allexport
+    source .env
+    set +o allexport
+else
+    echo "Error: .env file not found." >&2
+    exit 1
+fi
+
+
+config_file="./app/data/app_config.json"
+backup_file="${config_file}.bak"
+
+cleanup() {
+    if [[ -f "$backup_file"  ]]
+     then
+        mv "$backup_file" "$config_file"
+    fi
+}
+trap cleanup EXIT
+
+cp "$config_file" "$backup_file"
+
+jq --arg key "$apikey" '.apikey = $key' "$backup_file" > "$config_file"
+
+
+if [[ -f ./app/streamlit/Home.py ]]; then
+    python3 ./app/streamlit/Home.py
+else
+    echo "Error: Test entrypoint './app/streamlit/Home.py' not found." >&2
+    exit 1
+fi
+
